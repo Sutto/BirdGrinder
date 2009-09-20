a32246329df99f082d7c811b756b94af5e01ecba
Darcy Laycock <sutto@sutto.net>
2009-09-21 02:50:26 +0800

    Use basic file store for the moment

--- config/setup.rb
+++ config/setup.rb
@@ -1,6 +1,6 @@
 BirdGrinder::Loader.before_run do
   require 'moneta/basic_file'
-  store = Moneta::BasicFile.new(:namespace => 'bird_grinder', :path => BirdGrinder::Settings.root.join("data"))
+  store = Moneta::BasicFile.new(:namespace => 'bird_grinder', :path => File.join(BirdGrinder::Settings.root, "data"))
   BirdGrinder.cache_store = store
   
   DeceptionHandler.register!
