f760203663a5f5c20351f57155250c8ccecb6533
Darcy Laycock <sutto@sutto.net>
2009-09-21 05:23:20 +0800

    Vendor in moneta/redis

--- config/setup.rb
+++ config/setup.rb
@@ -1,8 +1,7 @@
+require 'moneta/redis'
+
 BirdGrinder::Loader.before_run do
-  require 'moneta/basic_file'
-  store = Moneta::BasicFile.new(:namespace => 'bird_grinder', :path => File.join(BirdGrinder::Settings.root, "data"))
-  BirdGrinder.cache_store = store
-  
+  BirdGrinder.cache_store = Moneta::Redis.new
   DeceptionHandler.register!
 end
 
