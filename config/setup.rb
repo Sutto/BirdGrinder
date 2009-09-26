02b9fe2c9984fc8d1e2221037d6fce5c135a0ee9
Darcy Laycock <sutto@sutto.net>
2009-09-26 12:01:51 +0800

    Fix default setup

--- config/setup.rb
+++ config/setup.rb
@@ -1,9 +1,5 @@
-require 'moneta/redis'
-
 BirdGrinder::Loader.before_run do
-  BirdGrinder.cache_store = Moneta::Memory.new
   DebugHandler.register!
-  #DeceptionHandler.register!
 end
 
 BirdGrinder::Loader.once_running do
