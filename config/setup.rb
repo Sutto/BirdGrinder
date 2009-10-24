ca107e6c6fa1f035fbd9a50034df3ba264b5f258
Darcy Laycock <sutto@sutto.net>
2009-10-24 12:03:06 +0800

    Bump version

--- config/setup.rb
+++ config/setup.rb
@@ -6,5 +6,5 @@ BirdGrinder::Loader.once_running do
   #BirdGrinder::QueueProcessor.start
   #BirdGrinder::Client.current.search "railsrumble"
   #BirdGrinder::Client.current.search "eotw", :repeat => true
-  BirdGrinder::Client.current.streaming.track 'facebook,#facebook,#fb'
+  #BirdGrinder::Client.current.streaming.track 'facebook,#facebook,#fb'
 end
